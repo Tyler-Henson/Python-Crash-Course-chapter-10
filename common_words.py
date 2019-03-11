9aba9042f82f85ff71babf6931d532901e05e467
Tyler H <47481160+Tyler-Henson@users.noreply.github.com>
2019-03-11 14:38:20 -0700

    update: working on 10-10

--- common_words.py
+++ common_words.py
@@ -3,15 +3,24 @@ Problem 10-10 of Python Crash Course
 """
 
 
-def word_count(book, word):
+def word_count(book, word='sawyer'):
     count = 0
+    lower_case_lines = []
+    try:
+        with open(book, 'r', encoding="utf8") as b:
+            lines = b.readlines()
+            for line in lines:
+                lower_case_lines.append(line.lower())
 
-    with open(book, 'r') as b:
-        lines = b.readlines()
-        #for line in lines:
-            #count += line.count(word)
-    #return count
+            for lower_case_line in lower_case_lines:
+                count += lower_case_line.count(word)
+        return count
+    except FileNotFoundError:
+        print("File not found")
+        return 0
 
 
 if __name__ == '__main__':
-    print(word_count('sawyer.txt', 'the'))
+    print(word_count('sawyer.txt'))
+    print(word_count('dogs.txt'))
+    print(word_count('saw.txt'))
