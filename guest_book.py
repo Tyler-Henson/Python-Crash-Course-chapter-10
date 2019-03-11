2e58cd53b7bea2c90c3a9a44a777d173a0a20ebc
Tyler H <47481160+Tyler-Henson@users.noreply.github.com>
2019-03-11 13:31:11 -0700

    update: finished 10-4

---
+++ guest_book.py
@@ -0,0 +1,12 @@
+"""
+Problem 10-4 of Python Crash Course
+"""
+
+filename = 'guest_book.txt'
+with open(filename, 'a') as file:
+    while True:
+        name = input("Sign the guest book please. enter 'f' to exit")
+        if name == 'f':
+            break
+        else:
+            file.write(name + '\n')
